9fd38ff059366372c3927975d32dc3c3626e9b20
Igor Bestuzhev <best.igor@gmail.com>
2010-05-13 23:42:49 +0300

    Named UrlConf

--- profileapp/views.py
+++ profileapp/views.py
@@ -1,8 +1,9 @@
 """ Create your views here. """
 # from django.http import HttpResponse
-from django.shortcuts import render_to_response
-from profileapp.models import UserProf
+from django.shortcuts import render_to_response,redirect
 from django.contrib.auth.models import User
+from django.core.exceptions import ObjectDoesNotExist
+from profileapp.models import UserProf
 
 def list_profile (request):
     """ This view is used to show the list of all possible users """
@@ -12,5 +13,9 @@ def list_profile (request):
 
 def show_profile (request, user):
     """ This view shows some information about selected user"""
+    try:
+        user_entry = User.objects.get(username=user)
+    except ObjectDoesNotExist:
+        return redirect('profileapp.views.list_profile',permanent=True)
     return render_to_response ('ShowProfile.html',
-                    {'user':User.objects.get(username=user).get_profile()})
+                    {'user':user_entry.get_profile()})
